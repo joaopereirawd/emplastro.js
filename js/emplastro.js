fe49d14a0330bed4dcf4cdc49bbfdd4cd8af7bcb
João Pereira <joaopereirawd@gmail.com>
2018-06-07 11:21:16 +0100

    UPD: version 1.5

--- js/emplastro.js
+++ js/emplastro.js
@@ -1,30 +1,89 @@
-	/*=========================================
-	 * emplastro.js: Version 1.0
+	/****************************************
+	 * emplastro.js: Version 1.5
 	 * author: João Pereira
 	 * website: http://www.joaopereira.pt
 	 * email: joaopereirawd@gmail.com
 	 * Licensed MIT 
-	=========================================*/
-	jQuery.fn.emplastro = function () {
-	    this.css("position", "absolute");
-	    var win_height = $(window).height(); 
-	    var win_outer = $(window).outerHeight(); 
-	    var win_scrollTop = $(window).scrollTop();
-	   	var object = $(this).height();
-	    	console.log(object);
+	*****************************************/
+jQuery.fn.emplastro = function( options ) {
+ 	
+	var emplastro = $(this);
 
-	    this.css("top", ((win_outer/2 - object/2 )) + $(window).scrollTop() + "px");
-	    return this;
+ 	var application =  {
+		init: function() { 
+			var object = emplastro;
+			var object_height = object.height();
+		    var win_height = $(window).height();
+		    var win_outer = $(window).outerHeight();
+		    var win_scrollTop = $(window).scrollTop();
+		    	object.css("top", ((win_outer/2 - object_height/2 )) + $(window).scrollTop() + "px"); //calc
+		}
+ 	}
+
+ 	//emplastro settings
+    var settings = $.extend( {
+    	position:'absolute',
+    	width:'',
+    	height:'',
+    	class:'',
+        backgroundColor: "#fff",
+        side:'right',
+        borderRadius:'0px',
+        boxShadow:'0px 3px 8px rgba(0, 0, 0, 0.28)',
+        transition:'all .2s',
+        gutter:10
+    }, options );
+
+
+	emplastro.css(
+		{	
+			'position':settings.position,
+			'width':settings.width,
+			'height':settings.height,
+			'background-color':settings.backgroundColor,
+			'border-radius':settings.borderRadius,
+			'box-shadow':settings.boxShadow,
+			'transition':settings.transition
+		}
+	);
+
+
+	var t =  emplastro;
+	console.log(emplastro);
+	
+	switch (settings.side) {
+	    case 'left':
+	        emplastro.css({'left':'0px'});
+	        	if(settings.gutter) {
+	        		emplastro.css({'margin-left':settings.gutter+'px'});
+	        	}
+	        break;
+	    case 'right':
+	    	emplastro.css({'right':'0px'});
+	        	if(settings.gutter) {
+	        		emplastro.css({'margin-right':settings.gutter+'px'});
+	        	}
+	    	break;
+	    default:
+	    	emplastro.css({'left':'0px'});
+	        	if(settings.gutter) {
+	        		emplastro.css({'margin-left':settings.gutter+'px'});
+	        	}
 	}
 
 
-	function teste(){
-		$('.teste').emplastro();
-	}
+    return this.each(function() {
+		$(document).ready(function(){
+			application.init();			    
+			$(window).scroll(function(){
+		        application.init();
+		    });
+		});
+    });
+ 
+};
 
-	$(document).ready(function(){
-		teste();
-	    $(window).scroll(function(){
-	        teste(); 
-	    });
-	});
+
+
+
+
